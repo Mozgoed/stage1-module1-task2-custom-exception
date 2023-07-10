9dd2d7e659067fd64dfb75bb33add98b2e021979
Кононов Алексей <aleksey.n.kononov@gazprombank.ru>
2023-07-10 13:04:08 +0300

    done

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,7 +5,11 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
+  public Student find(long studentID) throws StudentNotFoundException {
+    Student student = Student.getValueOf(studentID);
+    if (student == null) {
+      throw new StudentNotFoundException("Could not find student with ID " + studentID);
+    }
     return Student.getValueOf(studentID);
   }
 
